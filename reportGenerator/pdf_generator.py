e6b76dec80c8ae3f96f1bd357a8e55a1f795b015
pioszc4 <szczesnypiotr628@gmail.com>
2026-05-02 14:07:47 +0200

    (SCRUM-31)feat: removing usage of batches from pdf generator

--- reportGenerator/pdf_generator.py
+++ reportGenerator/pdf_generator.py
@@ -1,19 +1,18 @@
 from reportlab.platypus import SimpleDocTemplate, Paragraph
 from reportlab.lib.pagesizes import letter
-from eavesdropper.batching.main import get_next_batch
 
-def add_batch(merged_file, rml_string):
+def add_part(merged_file, rml_string):
     try:
         merged_file += rml_string
         return merged_file
     except Exception as e:
-        print(f"Error adding batch: {e}")
+        print(f"Error adding part: {e}")
 
-def loop_batch(batch_string):
+def loop_batch(text_string):
     merged_file = ""
-    while batch_string is not None:
-        merged_file = add_batch(merged_file, batch_string)
-        batch_string = get_next_batch()
+    while text_string is not None:
+        merged_file = add_part(merged_file, text_string)
+        
     return merged_file
     
 def generate_pdf(merged_file, output_file):
